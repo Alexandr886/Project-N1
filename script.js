b089b8841ccdeeb9c7c92a4d476a0ac2c9cb758f
Alexandr Yershov <69073713+Alexandr886@users.noreply.github.com>
2022-04-12 21:44:31 +0700

    Изменения: 1) Слайдер переделан - подключен свайпер и + в контенте слайдов изменена разметка в выборе цвета, стили и немного JS. 2) При открытой модалке страница не скроллится. 3) Изменен JS по склонению слов в круглых progress. 4) Добавлено событие touchend для горизонтального progress

--- script.js
+++ script.js
@@ -29,30 +29,39 @@ for (const circle of circles) {
 
    input.addEventListener('input', function () {
       setProgress(input.value);
+      //замена значений в круглом progress - кол-во лет
       years.innerHTML = (input.value / 100 * 10).toFixed(0);
-      if (years.innerHTML == 1) {
-         yearsInWords.innerHTML = 'Год'
-      }
-      else if (years.innerHTML == 2 || years.innerHTML == 3 || years.innerHTML == 4) {
-         yearsInWords.innerHTML = 'Года'
-      }
-      else {yearsInWords.innerHTML = 'Лет'};
+      yearsInWords.innerHTML = declOfNum((input.value / 100 * 10).toFixed(0), ['год', 'года', 'лет']);
 
+      //замена значений в круглом progress - кол-во проектов
       projects.innerHTML = (input.value / 100 * 100).toFixed(0);
-      if (projects.innerHTML == 1 || projects.innerHTML == 21 || projects.innerHTML == 31 || projects.innerHTML == 41 || projects.innerHTML == 51 || projects.innerHTML == 61 || projects.innerHTML == 71 || projects.innerHTML == 81 || projects.innerHTML == 91) {
-         projectsInWords.innerHTML = 'Проект'
-      }
-      else if (projects.innerHTML == 2 || projects.innerHTML == 3 || projects.innerHTML == 4 || projects.innerHTML == 22 || projects.innerHTML == 23 || projects.innerHTML == 24 || projects.innerHTML == 32 || projects.innerHTML == 33 || projects.innerHTML == 34 || projects.innerHTML == 42 || projects.innerHTML == 43 || projects.innerHTML == 44 || projects.innerHTML == 52 || projects.innerHTML == 53 || projects.innerHTML == 54 || projects.innerHTML == 62 || projects.innerHTML == 63 || projects.innerHTML == 64 || projects.innerHTML == 72 || projects.innerHTML == 73 || projects.innerHTML == 74 || projects.innerHTML == 82 || projects.innerHTML == 83 || projects.innerHTML == 84 || projects.innerHTML == 92 || projects.innerHTML == 93 || projects.innerHTML == 94) {
-         projectsInWords.innerHTML = 'Проекта'
-      }
-      else {projectsInWords.innerHTML = 'Проектов'}
+      projectsInWords.innerHTML = declOfNum((input.value / 100 * 100).toFixed(0), ['проект', 'проекта', 'проектов']);
+
    });
 }
+
+//функция для склонения слов в progress-кругах
+function declOfNum(n, text_arr) {
+   n = Math.abs(n) % 100
+   var n1 = n % 10
+   if (n > 10 && n < 20) {
+     return text_arr[2]
+   }
+   if (n1 > 1 && n1 < 5) {
+     return text_arr[1]
+   }
+   if (n1 == 1) {
+     return text_arr[0]
+   }
+   return text_arr[2]
+ }
 
 
 //--------------Работа с ползунком (slider-range) 
 input.addEventListener('mousemove', range);
 input.addEventListener('touchmove', range);
+input.addEventListener('click', range);
+input.addEventListener('touchend', range);
    
 function range() {
    let x = input.value;
@@ -62,12 +71,11 @@ function range() {
 
 
 //----------------выбор цвета у кроссовок
-const colors = document.querySelectorAll('.color__in');
+const colors = document.querySelectorAll('.color');
 
 for (let i = 0; i < colors.length; i++) {
    colors[i].addEventListener('click', function() {
-      let colorBorders = document.querySelectorAll('.color__out');
-      colorBorders[i].classList.toggle('color__out_active')
+      colors[i].classList.toggle('color-active');
    })
 }
 
@@ -83,76 +91,36 @@ for (let i = 0; i < likes.length; i++) {
 
 
 //-----------------слайдер
-const images = document.querySelectorAll('.card');
-const sliderLine = document.querySelector('.slider-line');
-let count = 0;
-let width = 270; //240px сама карточка товара + 30px это gap;
-let currentSlide = document.querySelector('.slider-info__current');
-
-function init() {
-   sliderLine.style.width = width * images.length + 'px';
-   rollSlider();
-}
-
-window.addEventListener('resize', init);
-init();
-
-document.querySelector('.slider-prev').addEventListener('click', function() {
-   count--;
-   if (count < 0) {
-      count = images.length - 1;
-   };
-
-   //вывод текущего слайда при нажатии на кнопку "предыдущий"
-   i = count + 1;
-   if (i < 10) {
-      currentSlide.innerHTML = '0' + i;
+new Swiper('.slider', {
+   //изначально показ только одно слайда и потом через breakpoints добавим
+   slidesPerView: '1',
+   //расстояние между слайдами
+   spaceBetween: 30,
+   //вывод пагинации
+   pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+      //clickable: true,
+   },
+   //кнопки-стрелки для прокрутки слайдера
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+   //чтобы срабатывало перетаскивание слайдов мышью.
+   simulateTouch: true,
+   //слайды круттся по скроллу колеса мыши
+   mousewheel: {
+      sensitivity: 1,
+   },
+   //бесконечная прокрутка слайдов
+   loop: true,
+   breakpoints: {
+      768: {slidesPerView: '2',},
+      1024: {slidesPerView: '3',},
+      1440: {slidesPerView: '4',},
    }
-   else {
-      currentSlide.innerHTML = i;
-   }
-
-   rollSlider();
-})
-
-document.querySelector('.slider-next').addEventListener('click', function() {
-   count++;
-   if (count >= images.length) {
-      count = 0;
-   }
-
-   //вывод текущего слайда при нажатии на кнопку "следующий"
-   i = count + 1;
-   if (i < 10) {
-      currentSlide.innerHTML = '0' + i;
-   }
-   else {
-      currentSlide.innerHTML = i;
-   }
-
-   rollSlider();
-})
-
-function rollSlider() {
-   sliderLine.style.transform = 'translate(-' + count*width + 'px)';
-}
-
-//вывод текущего слайда при загрузке страницы
-if (count < 10) {
-   currentSlide.innerHTML = '0' + (count + 1);
-}
-else {
-   currentSlide.innerHTML = count + 1;
-}
-
-//--------вывод кол-ва слайдов
-const lastSlider = document.querySelector('.slider-info__last');
-if (images.length < 10) {
-   lastSlider.innerHTML = '0' + images.length;
-}
-else {
-   lastSlider.innerHTML = images.length;
-}
+});
 
 
 //---------------------------------МОДАЛКА для index.html
@@ -163,32 +131,27 @@ const overlay = document.getElementById('overlay');
 openModalButtons.forEach (button => {
    button.addEventListener('click', () => {
       const modal = document.querySelector(button.dataset.modalTarget)
-      openModal(modal);
+      openCloseModal(modal);
    })
 })
 
 overlay.addEventListener('click', () => {
    const modals = document.querySelectorAll('.modal.active');
    modals.forEach(modal => {
-         closeModal(modal);
+      openCloseModal(modal);
    })
 })
 
 closeModalButtons.forEach(button => {
    button.addEventListener('click', () => {
       const modal = button.closest('.modal');
-      closeModal(modal);
+      openCloseModal(modal);
    })
 })
 
-function openModal(modal) {
+function openCloseModal(modal) {
    if (modal == null) return
-   modal.classList.add('active');
-   overlay.classList.add('active');
-}
-
-function closeModal(modal) {
-   if (modal == null) return
-   modal.classList.remove('active');
-   overlay.classList.remove('active');
+   modal.classList.toggle('active');
+   overlay.classList.toggle('active');
+   document.body.classList.toggle('block')
 }
